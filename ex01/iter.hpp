ac8b92b90c567e1c77cace32f63ff171251b36fd
Quah Xuanyu <xuanthegreat@gmail.com>
2026-04-28 21:15:57 +0800

    changed iter.hpp to combine into a single function

--- ex01/iter.hpp
+++ ex01/iter.hpp
@@ -3,26 +3,11 @@
 
 #include <stdlib.h>
 
-// For non-const arrays with non-const functions
-template <typename T>
-void iter(T* array, const size_t length, void (*func)(T&)) {
-    for (size_t i = 0; i < length; ++i) {
-        func(array[i]);
-    }
-}
-
-// For non-const arrays with const functions
-template <typename T>
-void iter(T* array, const size_t length, void (*func)(const T&)) {
-    for (size_t i = 0; i < length; ++i) {
-        func(array[i]);
-    }
-}
-
-// For const arrays with const functions
-template <typename T>
-void iter(const T* array, const size_t length, void (*func)(const T&)) {
-    for (size_t i = 0; i < length; ++i) {
+template <typename T, typename Func>
+void iter(T *array, const size_t length, Func func)
+{
+    for (size_t i = 0; i < length; ++i)
+    {
         func(array[i]);
     }
 }
